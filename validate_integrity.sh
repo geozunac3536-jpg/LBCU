71799e596d147e7f1f3d0b40f996c27de6bef326
Genaro Carrasco Ozuna <geozunac3536@gmail.com>
2025-10-28 00:16:59 -0700

    Delete validate_integrity.sh

--- validate_integrity.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash# Script para verificar integridad de PDFs
-
-echo "Generando checksums SHA256..."
-sha256sum *.pdf > checksums.txt
-echo "Checksums guardados en checksums.txt"
-echo "Para verificar integridad: sha256sum -c checksums.txt"
